ff0ec22fc58c414273f39611572cdabed682b7af
Subhash Naval <subhash.naval@gmail.com>
2020-12-20 15:42:28 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,46 @@
+var bullet, speed, weight ;
+var wall, thickness;
+
+function setup(){
+  createCanvas(1600,400);
+
+  bullet= createSprite(50,200,40,30);
+  bullet.shapeColor=color(255); 
+
+  wall = createSprite(1500,200,thickness,height/2);
+ speed = random(223,321);
+ weight = random(30,52);
+ thickness =random(22,83);
+ bullet.velocityX = speed;
+
+}
+
+function draw() {
+  background(0);  
+  if(hasCollided(bullet,wall)){
+    bullet.velocityX = 0;
+    var damage = 0.5 * weight* speed * speed/ (thickness * thickness * thickness);
+
+    if(damage>10)
+    {
+      wall.shapeColor = color(255,0,0);
+    }
+    if(damage<10)
+    {
+      wall.shapeColor = color(0,255,0);
+    }
+    
+  }
+ 
+ drawSprites();
+}
+function hasCollided(lbullet, lwall){
+  bulletRightEdge = lbullet.x +lbullet.width;
+  wallLeftEdge = lwall.x;
+  if(bulletRightEdge >= wallLeftEdge)
+  {
+    return true;
+  }
+  return false;
+}
+
